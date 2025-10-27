c48ec84d3f221a0455324410ecf60040b19240c4
luchomarche100 <luciano.marchese25@gmail.com>
2025-10-27 20:35:13 -0300

    se agregan dependencias y estructura

---
+++ src/main/java/com/diseno/tpDiseno/TpDisenoApplication.java
@@ -0,0 +1,13 @@
+package com.diseno.tpDiseno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDisenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDisenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diseno/tpDiseno/TpDisenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diseno.tpDiseno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpDisenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
